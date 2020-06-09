cb701f65477d7177d7cf8e46cb9ce331a9731a12
taiyunho <66684418+taiyunho@users.noreply.github.com>
2020-06-09 22:51:50 +0800

    Add files via upload

---
+++ First trial project/event.js
@@ -0,0 +1,52 @@
+
+        window.sr = ScrollReveal();
+        sr.reveal('.section1', {
+            duration: 2000,
+            origin: 'left',
+            distance: '200px',
+            ViewFactor:0.1
+        })
+
+        sr.reveal('.section2', {
+            duration: 2000,
+            origin: 'right',
+            distance: '200px',
+            ViewFactor:0.2
+
+        })
+
+        sr.reveal('.section3', {
+            duration: 2000,
+            origin: 'bottom',
+            distance: '200px',
+            ViewFactor:0.5
+        })
+
+        sr.reveal('.section4', {
+            duration: 2000,
+            origin: 'left',
+            distance: '200px',
+            ViewFactor:0.2
+        })
+
+        sr.reveal('.section5', {
+            duration: 2000,
+            origin: 'bottom',
+            distance: '200px',
+            ViewFactor:1
+        })
+
+        sr.reveal('.section6', {
+            duration: 2000,
+            origin: 'right',
+            distance: '200px',
+            ViewFactor:1
+        })
+
+        sr.reveal('.section7', {
+            duration: 2000,
+            origin: 'left',
+            distance: '200px',
+            ViewFactor:1
+        })
+   
